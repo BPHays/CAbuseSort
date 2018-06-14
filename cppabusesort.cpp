2dbf406194e306f12573ceb61dbdd40e9b316872
RowanBHart <Rowan.Hart@ngc.com>
2018-06-14 12:12:10 -0400

    In which our hero cleans up his mess

--- cppabusesort.cpp
+++ cppabusesort.cpp
@@ -9,20 +9,7 @@ github.com/bphays/CAbuseSort
 
 template<typename T>
 void qs(T a, int s) {
-	for (
-			int i = 0, w = 0;
-			s > 1 &&
-			(i < s-1 || (qs(a, w), qs(a+w+1, s-w-1), 0));
-			((a[i] < a[s - 1] ?
-			  (a[i] != a[w] ?
-			   a[w] ^= a[i], a[i] ^= a[w], a[w] ^= a[i], w++ :
-			   w++):
-			  1),
-			 i++),
-			i == s-1 ?
-			(a[w] != a[s-1] ? a[s-1] ^= a[w], a[w] ^= a[s-1], a[s-1] ^= a[w], 1:
-			 1) :
-			1);
+	for (int i = 0, w = 0; s > 1 && (i < s - 1 || (qs(a, w), qs(a + w+ 1, s - w - 1), 0)); ((a[i] < a[s - 1] ? (a[i] != a[w] ? a[w] ^= a[i], a[i] ^= a[w], a[w] ^= a[i], w++ : w++) : 1), i++), i == s - 1 ? (a[w] != a[s - 1] ? a[s - 1] ^= a[w], a[w] ^= a[s - 1], a[s - 1] ^= a[w], 1 : 1) : 1);
 }
 
 int main() {
