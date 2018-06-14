bd5aada0a65ea16aaeaf07c6915829d4c44bc5fe
RowanBHart <Rowan.Hart@ngc.com>
2018-06-14 15:59:01 -0400

    in which our hero makes caboose sort work with ANYTHING!

--- cppabusesort.cpp
+++ cppabusesort.cpp
@@ -7,13 +7,49 @@ github.com/bphays/CAbuseSort
 #endif
 
 template<typename T>
-void qs(T a, int s) {
-	for (int i = 0, w = 0; s > 1 && (i < s - 1 || (qs(a, w), qs(a + w+ 1, s - w - 1), 0)); ((a[i] < a[s - 1] ? (a[i] != a[w] ? a[w] ^= a[i], a[i] ^= a[w], a[w] ^= a[i], w++ : w++) : 1), i++), i == s - 1 ? (a[w] != a[s - 1] ? a[s - 1] ^= a[w], a[w] ^= a[s - 1], a[s - 1] ^= a[w], 1 : 1) : 1);
+void qs2(T a, int s) {
+	auto t = *a;
+	for (int i = 0, w = 0; s > 1 && (i < s - 1 || (qs2(a,w), qs2(a + w + 1, s - w - 1), 0)); ((a[i] < a[s - 1] ? (a[i] != a[w] ? t = a[i], a[i] = a[w], a[w] = t, w++ : w++) : 1), i++), i == s - 1 ? (a[w] != a[s - 1] ? t = a[w], a[w] = a[s - 1], a[s - 1] = t, 1 : 1) : 1);
 }
 
 int main() {
-	std::vector<char> a = {'f','a','c','k','e','m','i','n','e','m','g','e','r','b','i','l','s'};
-	std::vector<char>::iterator it = a.begin();
-	qs(it, a.size());
-	for (auto i : a) std::cout << i << std::endl;
+	std::vector<std::string> s = {
+			"Sophia","Jackson"
+			,"Emma","Aiden"
+			,"Olivia","Lucas"
+			,"Ava","Liam"
+			,"Mia","Noah"
+			,"Isabella","Ethan"
+			,"Riley","Mason"
+			,"Aria","Caden"
+			,"Zoe","Oliver"
+			,"Charlotte","Elijah"
+			,"Lily","Grayson"
+			,"Layla","Jacob"
+			,"Amelia","Michael"
+			,"Emily","Benjamin"
+			,"Madelyn","Carter"
+			,"Aubrey","James"
+			,"Adalyn","Jayden"
+			,"Madison","Logan"
+			,"Chloe","Alexander"
+	};
+	std::vector<std::vector<std::string>> v = {{"hello", "I", "am", "a", "vector"}, {"And", "I", "am", "one", "too"}, {"Vector", "number", "three", "here"}};
+
+	std::vector<int> k = {5, 32, 9, 29, 1, 9, 3, 1, 9, 14, 2, 6, 2, 7, 8, 8 ,291, 38, 48, 2834,325,3465346,32542,4,21342,353,6,457,456345,-40};
+	
+	std::vector<std::string>::iterator it2 = s.begin();
+	qs2(it2, s.size());
+	for (auto i : s) std::cout << i <<  std::endl;
+	std::vector<std::vector<std::string>>::iterator it3 = v.begin();
+	qs2(it3, v.size());
+	for (auto i : v) {
+		for (auto c : i) {
+			std::cout << c << std::endl;
+		}
+	}
+	std::vector<int>::iterator it4 = k.begin();
+	qs2(it4, k.size());
+	for (auto i : k) std::cout << i << std::endl;
+
 }
